4043e576265cdbb690f0b6a3b6a3373088026b68
cabrera-carlos <cabrera.crcc@gmail.com>
2022-09-04 23:11:25 -0400

    Completing project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
+import { useState } from "react";
+import reviews from "./data";
 
 function App() {
+  const [index, setIndex] = useState(0);
+  const { id, image, job, name, text } = reviews[index];
+
+  function calcIndex(i) {
+    if (i > reviews.length - 1) {
+      return 0;
+    }
+
+    if (i < 0) {
+      return reviews.length - 1;
+    }
+
+    return i;
+  }
+
+  function randomIndex() {
+    let rand = Math.floor(Math.random() * reviews.length);
+
+    if (rand === index) {
+      rand = calcIndex(rand + 1);
+    }
+
+    return rand;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section className="container">
+        <div className="title">
+          <h2>our reviews</h2>
+          <div className="underline"></div>
+        </div>
+        <article className="review">
+          <div className="img-container">
+            <img src={image} alt={name} className="person-img" />
+            <span className="quote-icon">
+              <FaQuoteRight />
+            </span>
+          </div>
+          <h4 className="author">{name}</h4>
+          <p className="job">{job}</p>
+          <p className="info">{text}</p>
+          <div className="button-container">
+            <button
+              className="prev-btn"
+              onClick={() => setIndex(calcIndex(index - 1))}
+            >
+              <FaChevronLeft />
+            </button>
+            <button
+              className="next-btn"
+              onClick={() => setIndex(calcIndex(index + 1))}
+            >
+              <FaChevronRight />
+            </button>
+          </div>
+          <div className="button-container">
+            <button
+              className="random-btn"
+              onClick={() => setIndex(randomIndex())}
+            >
+              surprise me
+            </button>
+          </div>
+        </article>
+      </section>
+    </main>
   );
 }
 
